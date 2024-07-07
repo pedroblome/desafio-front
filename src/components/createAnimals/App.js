aa200e047972b5bfb0498a49b68dec2b8d610636
pedroblome <joaopedroblome78@gmail.com>
2024-07-07 00:57:08 -0300

    bootstrap em pagina de listagem e cadastro de animais

--- src/components/createAnimals/App.js
+++ src/components/createAnimals/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './App.css';
 import TopBar from '../topBar/App';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CadastrarAnimal = () => {
   const navigate = useNavigate();
@@ -12,20 +11,9 @@ const CadastrarAnimal = () => {
   const [nomeCategoria, setNomeCategoria] = useState('');
   const [dataNascimento, setDataNascimento] = useState('');
   const [status, setStatus] = useState('DISPONIVEL');
-  const [showPopup, setShowPopup] = useState(false)
-  
-  const clearForm = () => {
-    setNome('');
-    setDescricao('');
-    setUrlImagem('');
-    setNomeCategoria('');
-    setDataNascimento('');
-    setStatus('DISPONIVEL');
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     const newAnimal = {
       nome,
       descricao,
@@ -45,50 +33,49 @@ const CadastrarAnimal = () => {
       .then(response => response.json())
       .then(data => {
         console.log('Animal cadastrado com sucesso:', data);
-        //aparece pop up dizendo que criou adcinou o novo dado! opção de cadastrar outro animal ou ok.
-        navigate("/")
+        navigate("/");
       })
       .catch(error => console.error('Error cadastrando animal:', error));
   };
 
   return (
     <div>
-      <TopBar/>
-      <div className="cadastrar-animal">
-      <h1>Cadastrar Animal</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Nome:
-          <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
-        </label>
-        <label>
-          Descrição:
-          <input type="text" value={descricao} onChange={(e) => setDescricao(e.target.value)} required />
-        </label>
-        <label>
-          URL da Imagem:
-          <input type="text" value={urlImagem} onChange={(e) => setUrlImagem(e.target.value)} required />
-        </label>
-        <label>
-          Categoria:
-          <input type="text" value={nomeCategoria} onChange={(e) => setNomeCategoria(e.target.value)} required />
-        </label>
-        <label>
-          Data de Nascimento:
-          <input type="date" value={dataNascimento} onChange={(e) => setDataNascimento(e.target.value)} required />
-        </label>
-        <label>
-          Status:
-          <select value={status} onChange={(e) => setStatus(e.target.value)} required>
-            <option value="DISPONIVEL">Disponível</option>
-            <option value="ADOTADO">Adotado</option>
-          </select>
-        </label>
-        <button type="submit" onClick={handleSubmit}>Cadastrar</button>
-      </form>
+      <TopBar />
+      <div className="container mt-5">
+        <h1 className="mb-3">Cadastrar Animal</h1>
+        <form className="w-50 mx-auto" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="nome" className="form-label">Nome:</label>
+            <input type="text" className="form-control" id="nome" value={nome} onChange={e => setNome(e.target.value)} required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="descricao" className="form-label">Descrição:</label>
+            <input type="text" className="form-control" id="descricao" value={descricao} onChange={e => setDescricao(e.target.value)} required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="urlImagem" className="form-label">URL da Imagem:</label>
+            <input type="text" className="form-control" id="urlImagem" value={urlImagem} onChange={e => setUrlImagem(e.target.value)} required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="nomeCategoria" className="form-label">Categoria:</label>
+            <input type="text" className="form-control" id="nomeCategoria" value={nomeCategoria} onChange={e => setNomeCategoria(e.target.value)} required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="dataNascimento" className="form-label">Data de Nascimento:</label>
+            <input type="date" className="form-control" id="dataNascimento" value={dataNascimento} onChange={e => setDataNascimento(e.target.value)} required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="status" className="form-label">Status:</label>
+            <select className="form-select" id="status" value={status} onChange={e => setStatus(e.target.value)} required>
+              <option value="DISPONIVEL">Disponível</option>
+              <option value="ADOTADO">Adotado</option>
+            </select>
+          </div>
+          <button type="submit" className="btn btn-success">Cadastrar</button>
+        </form>
+      </div>
     </div>
-    </div>
-    
+
   );
 };
 
--- src/components/listAnimals/App.js
+++ src/components/listAnimals/App.js
@@ -1,30 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import TopBar from '../topBar/App';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o Bootstrap
+import './App.css'; // Importa os estilos CSS adicionais
+
 const ListAnimals = () => {
   const [animals, setAnimals] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8080/api/animals')
       .then(response => response.json())
-      .then(data => setAnimals(data))
+      .then(data => {
+        const sortedAnimals = sortAnimals(data);
+        setAnimals(sortedAnimals);
+      })
       .catch(error => console.error('Error fetching animals:', error));
   }, []);
 
   const handleChangeStatus = (id) => {
-    fetch(`http://localhost:8080/api/animals/changeStatus/${id}`, {
-      method: 'PUT'
-    })
+    fetch(`http://localhost:8080/api/animals/changeStatus/${id}`, { method: 'PUT' })
       .then(response => {
         if (response.ok) {
           setAnimals(prevAnimals =>
-            prevAnimals.map(animal =>
-              animal.id === id ? { ...animal, status: animal.status === 'DISPONIVEL' ? 'ADOTADO' : 'DISPONIVEL' } : animal
+            sortAnimals(
+              prevAnimals.map(animal =>
+                animal.id === id ? { ...animal, status: animal.status === 'DISPONIVEL' ? 'ADOTADO' : 'DISPONIVEL' } : animal
+              )
             )
           );
         }
       })
       .catch(error => console.error('Error changing status:', error));
+  };
+
+  const sortAnimals = (animals) => {
+    return animals.sort((a, b) => (a.status === 'DISPONIVEL' ? -1 : 1));
   };
 
   const availableAnimals = animals.filter(animal => animal.status === 'DISPONIVEL');
@@ -33,46 +42,43 @@ const ListAnimals = () => {
   return (
     <div>
       <TopBar />
-      <h1>Animais Disponíveis</h1>
-      <div className="animal-list">
-        {availableAnimals.map(animal => (
-          <div key={animal.id} className="animal-item">
-            <img src={animal.urlImagem} alt={animal.nome} width="200" />
-            <h2>{animal.nome}</h2>
-            <p>{animal.descricao}</p>
-            <p>Categoria: {animal.nomeCategoria}</p>
-            <p>Data de Nascimento: {animal.dataNascimento}</p>
-            <p>Idade: {animal.idade} anos</p>
-            <button
-              className={`button-status ${animal.status === 'DISPONIVEL' ? 'disponivel' : 'adotado'}`}
-              onClick={() => handleChangeStatus(animal.id)}
-            >
-              {animal.status}
-            </button>
-          </div>
-        ))}
-      </div>
-      <h1>Animais Adotados</h1>
-      <div className="animal-list">
-        {adoptedAnimals.map(animal => (
-          <div key={animal.id} className="animal-item">
-            <img src={animal.urlImagem} alt={animal.nome} width="200" />
-            <h2>{animal.nome}</h2>
-            <p>{animal.descricao}</p>
-            <p>Categoria: {animal.nomeCategoria}</p>
-            <p>Data de Nascimento: {animal.dataNascimento}</p>
-            <p>Idade: {animal.idade} anos</p>
-            <button
-              className={`button-status ${animal.status === 'DISPONIVEL' ? 'disponivel' : 'adotado'}`}
-              onClick={() => handleChangeStatus(animal.id)}
-            >
-              {animal.status}
-            </button>
-          </div>
-        ))}
+      <div className="container my-5">
+        <h1>Animais Disponíveis</h1>
+        <div className="row">
+          {availableAnimals.map(animal => (
+            renderAnimalCard(animal)
+          ))}
+        </div>
+        <h1>Animais Adotados</h1>
+        <div className="row">
+          {adoptedAnimals.map(animal => (
+            renderAnimalCard(animal)
+          ))}
+        </div>
       </div>
     </div>
   );
 };
+
+const renderAnimalCard = (animal) => (
+  <div key={animal.id} className="col-md-4 col-sm-6 col-xs-12 mb-4">
+    <div className="card">
+      <img src={animal.urlImagem} className="card-img-top" alt={animal.nome} />
+      <div className="card-body">
+        <h5 className="card-title">{animal.nome}</h5>
+        <p className="card-text">{animal.descricao}</p>
+        <p className="card-text"><small>Categoria: {animal.nomeCategoria}</small></p>
+        <p className="card-text"><small>Data de Nascimento: {animal.dataNascimento}</small></p>
+        <p className="card-text"><small>Idade: {animal.idade} anos</small></p>
+        <button
+          className={`btn ${animal.status === 'DISPONIVEL' ? 'btn-success' : 'btn-danger'}`}
+          onClick={() => handleChangeStatus(animal.id)}
+        >
+          {animal.status}
+        </button>
+      </div>
+    </div>
+  </div>
+);
 
 export default ListAnimals;
